80964a23018723e97b2560ceeaf2624d85cdbddb
Your Name <you@example.com>
2025-12-07 11:04:45 -0300

    feat(ui): add bar component
    - add bar with buttons: scratch, save, new, list, upload bg, quit
    - buttons are not functional yet
    - display it at the top

---
+++ src/components/bar.tsx
@@ -0,0 +1,15 @@
+
+function Bar() {
+  return (
+    <div role='toolbar'>
+      <button>[scratch]</button>
+      <button>[save]</button>
+      <button>[new]</button>
+      <button>[list]</button>
+      <button>[upload bg]</button>
+      <button>[quit]</button>
+    </div>
+  )
+}
+
+export default Bar;
