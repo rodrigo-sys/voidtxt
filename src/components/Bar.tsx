bbbed12d38e4ab7f5ad8e894f03a1edf05620188
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2025-12-22 14:10:40 -0300

    feat: implement note saving functionality
    - Enable saving notes via save button of Bar
    - Generate a random name if note does not have one
    - Properly update name prop of NoteContext object
    - Add recursive write permissions to app local data in capabilities
    
    Introduces the ability to persist note content to disk, completing the basic note editor workflow.

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,4 +1,8 @@
+import { appLocalDataDir, join } from "@tauri-apps/api/path";
+import { writeTextFile } from "@tauri-apps/plugin-fs";
 import { useNavigate } from "react-router-dom";
+import { NoteContext } from "../noteContext";
+import { useContext } from "react";
 
 const styles = {
   button: {
@@ -15,15 +19,32 @@ const styles = {
 
 function Bar() {
   const navigate = useNavigate()
+  const noteContext = useContext(NoteContext);
 
   function newNote() {
     navigate('/note?content=')
   }
 
+  async function saveNote() {
+    if (!noteContext) return;
+
+    const { content } = noteContext;
+    const note_name = noteContext.name || crypto.randomUUID().slice(0, 8) + '.md'
+
+    if (!noteContext.name) {
+      noteContext.setName(note_name)
+    }
+
+    const notes_path = await join(await appLocalDataDir(), 'notes')
+    const note_path = await join(notes_path, note_name)
+
+    await writeTextFile(note_path, content)
+  }
+
   return (
     <div style={styles.toolbar} role='toolbar'>
       <button style={styles.button}>[scratch]</button>
-      <button style={styles.button}>[save]</button>
+      <button style={styles.button} onClick={saveNote}>[save]</button>
       <button style={styles.button} onClick={newNote}>[new]</button>
       <button style={styles.button}>[list]</button>
       <button style={styles.button}>[upload bg]</button>
