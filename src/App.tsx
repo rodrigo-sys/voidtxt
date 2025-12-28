a738010577723b7e0e703329f081398a06d63015
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2025-12-28 12:33:12 -0300

    feat: allow users to set a custom image background
    - Use tauri dialog plugin to pick the image
    - Setup CSP and asset protocol for propper permissions
    - Overwrite css custom prop that save app image background

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -5,8 +6,21 @@ import Home from "./components/Home";
 import NoteEditorPage from "./pages/NoteEditorPage";
 import RootLayout from "./pages/layouts/RootLayout";
 import { List } from "./components/List";
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { appLocalDataDir, join } from "@tauri-apps/api/path";
+import { exists } from "@tauri-apps/plugin-fs";
 
 function App() {
+  useEffect(() => {
+    async function setBackgroundImage() {
+      const image_path = await join(await appLocalDataDir(), 'background-image')
+      if (!(await exists(image_path))) { return }
+      const image_url = convertFileSrc(image_path)
+      document.documentElement.style.setProperty('--app-image', `url("${image_url}")`)
+    }
+    setBackgroundImage()
+  }, [])
+
   return (
     <Routes>
       <Route element={<RootLayout />}>
--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,8 +1,10 @@
-import { join } from "@tauri-apps/api/path";
-import { writeTextFile } from "@tauri-apps/plugin-fs";
+import { appLocalDataDir, join } from "@tauri-apps/api/path";
+import { copyFile, writeTextFile } from "@tauri-apps/plugin-fs";
 import { useNavigate } from "react-router-dom";
 import { NoteContext } from "../noteContext";
 import { useContext } from "react";
+import { open } from "@tauri-apps/plugin-dialog";
+import { convertFileSrc } from "@tauri-apps/api/core";
 
 const styles = {
   button: {
@@ -49,13 +51,29 @@ function Bar() {
     navigate('/scratch')
   }
 
+  async function uploadBg() {
+    const file_path = await open({
+      multiple: false,
+      directory: false,
+    })
+
+    if (!file_path) { return }
+
+    const image_path = await join(await appLocalDataDir(), 'background-image')
+    await copyFile(file_path, image_path);
+
+    const cache_bust = `?t=${Date.now()}`
+    const image_url = convertFileSrc(image_path) + cache_bust
+    document.documentElement.style.setProperty('--app-image', `url("${image_url}")`)
+
+  }
   return (
     <div style={styles.toolbar} role='toolbar'>
       <button style={styles.button} onClick={showScratchFile}>[scratch]</button>
       <button style={styles.button} onClick={saveNote}>[save]</button>
       <button style={styles.button} onClick={newNote}>[new]</button>
       <button style={styles.button} onClick={showNotesList}>[list]</button>
-      <button style={styles.button}>[upload bg]</button>
+      <button style={styles.button} onClick={uploadBg}>[upload bg]</button>
       <button style={styles.button}>[quit]</button>
     </div>
   )
