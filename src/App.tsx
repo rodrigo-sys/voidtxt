ad7477656373014fecfe48409e4617203351c854
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-01-10 22:09:36 -0300

    feat: set 'new note' as home

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
 import NoteEditorPage from "./pages/NoteEditorPage";
 import RootLayout from "./pages/layouts/RootLayout";
 import { List } from "./components/List";
@@ -32,7 +31,7 @@ function App() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<NoteEditorPage/>} />
         <Route path='/note' element={<NoteEditorPage />} />
         <Route path='/notes' element={<List />} />
         <Route path='/scratch' element={<Navigate to='/note?filename=scratch.md' />} />
